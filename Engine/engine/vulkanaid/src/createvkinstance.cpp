af2dcbe8cad690e59209c88f753a824cb55807a7
yamin <yaminahmed821@gmail.com>
2025-12-22 14:29:31 +0100

    setting API version to 1.4 (on linux validation layer package haad version 1.3 watch out for this)

--- Engine/engine/vulkanaid/src/createvkinstance.cpp
+++ Engine/engine/vulkanaid/src/createvkinstance.cpp
@@ -33,7 +33,7 @@ void createVkInstance(VkInstance& instance)
     appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
     appInfo.pEngineName = "HATE Engine";
     appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.apiVersion = VK_API_VERSION_1_0;
+    appInfo.apiVersion = VK_MAKE_API_VERSION(0, 1, 4, 0);
 
     VkInstanceCreateInfo createInfo{};
     createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
