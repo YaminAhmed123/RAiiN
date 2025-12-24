52b4327a1a6d68024897e3d0b995bc484d43ca07
yamin <yaminahmed821@gmail.com>
2025-12-24 10:16:50 +0100

    saving work state for today.

---
+++ Engine/engine/vulkanaid/include/setupvkswapchain.hpp
@@ -0,0 +1,20 @@
+#ifndef SETUPVKSWAPCHAIN_HPP
+#define SETUPVKSWAPCHAIN_HPP
+#include <vulkan/vulkan.h>
+#include <vector>
+
+/*
+* Copyright (c) 2025 CortexR7
+* 
+* This header is for Swapchain creation
+*/
+
+
+
+typedef struct {
+    VkSurfaceCapabilitiesKHR capabilities;
+    std::vector<VkSurfaceFormatKHR> formats;
+    std::vector<VkPresentModeKHR> presentModes;
+} SwapChainSupportDetails;
+
+#endif
