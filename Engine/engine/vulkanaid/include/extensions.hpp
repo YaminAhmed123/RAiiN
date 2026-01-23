df8d098860d44a4f08121615f220426e20775159
Yamin Ahmed <yaminahmed821@gmail.com>
2026-01-23 16:40:33 +0100

    adding slang as minimal (maybe primary) shader language.

--- Engine/engine/vulkanaid/include/extensions.hpp
+++ Engine/engine/vulkanaid/include/extensions.hpp
@@ -10,7 +10,8 @@
 * This header defines device extensions
 */
 std::vector<const char*> deviceExtensions = {
-    VK_KHR_SWAPCHAIN_EXTENSION_NAME
+    VK_KHR_SWAPCHAIN_EXTENSION_NAME,
+    VK_KHR_SHADER_DRAW_PARAMETERS_EXTENSION_NAME
 };
 
 #endif
