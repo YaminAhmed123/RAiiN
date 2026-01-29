0fb9ee6657e3dcef7c525aae586ae74b170b2816
CortexR7 <128260156+YaminAhmed123@users.noreply.github.com>
2026-01-29 21:03:00 +0100

    Clean up whitespace in create_commandpoll.cpp
    Removed unnecessary blank lines in create_commandpoll.cpp

--- Engine/engine/render_engine/src/create_commandpoll.cpp
+++ Engine/engine/render_engine/src/create_commandpoll.cpp
@@ -12,12 +12,8 @@ void RenderEngine::createCommandPool()
     poolInfo.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
     poolInfo.queueFamilyIndex = queueFamilyIndices.graphicsFamily.value();
 
-
     if (vkCreateCommandPool(RenderEngine::device, &poolInfo, nullptr, &commandPool) != VK_SUCCESS)  // commandPool means RenderEngine::commandPool
     {
         throw std::runtime_error("failed to create command pool!");
     }
-
-
-
 }
